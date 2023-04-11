4e20937024ae6373154a76c324a95600c4c1c25a
salom <salom@gmail.com>
2023-04-11 05:43:23 +0500

    Home page tamom bo'ldi

--- js/main.js
+++ js/main.js
@@ -5,11 +5,41 @@ let bars = document.getElementById('bars')
 let leftContent = document.getElementById('left')
 let mark = document.getElementById('mark')
 
+let box1 = document.querySelectorAll('#box1')
+let linkICon = document.getElementById('linkICon')
+let link = document.getElementById('ver')
+let chewronICon = document.getElementById('chewronICon')
+
+linkICon.addEventListener('click', e => {
+    box1.forEach((box)=> {
+        box.style.display = 'block'
+    })
+
+    link.innerHTML = `
+    <a href="#" id="ver">Свернуть</a>
+    `
+    linkICon.style.display = 'none'
+    chewronICon.style.display = 'block'
+})
+
+chewronICon.addEventListener('click' , e => {
+    box1.forEach((box)=> {
+        box.style.display = 'none'
+    })
+    link.innerHTML = `
+    <a href="#" id="ver">Развернуть</a>
+    `
+    linkICon.style.display = 'block'
+    chewronICon.style.display = 'none'
+})
+
+
 bars.addEventListener('click' , e => {
     leftContent.style.height = '100vh'
     leftContent.style.transition = '2s'
 })
-
 mark.addEventListener('click' , e => {
     leftContent.style.height = '0vh'
 })
+
+
